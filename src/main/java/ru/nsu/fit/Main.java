bfdc8eef86a2a25b420d3273df7ae9dc878ef8e1
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2024-02-27 14:37:33 +0700

    Add example code for arg matchers

--- src/main/java/ru/nsu/fit/Main.java
+++ src/main/java/ru/nsu/fit/Main.java
@@ -10,15 +10,24 @@ import ru.nsu.fit.testclasses.SayHello;
 public class Main {
     public static void main(String[] args) {
         SayHello test = Dymock.burn(SayHello.class);
-        LeafMatcher[] arguments = {Dymock.green(), Dymock.green()};
+        LeafMatcher[] zeroArg = {};
+
+        Integer num = 2<<10;
+        LeafMatcher[] anyArg = {Dymock.green()};
+        LeafMatcher[] eqABC = {Dymock.yellow(num)};
+        LeafMatcher[] linkEqABC = {Dymock.red(num)};
         BonfireBuilder.buildBonfire(test)
-                .addStick(new Stick("sayHello", arguments, "Mocked!"))
-                .addStick(new Stick("returnInt", arguments, 42))
-                .addStick(new Stick("testArgs", arguments, 42))
-                .addStick(new Stick("testVoid", arguments, 42));
+                .addStick(new Stick("sayHello", zeroArg, "Mocked!"))
+                .addStick(new Stick("returnInt", zeroArg, 42))
+                .addStick(new Stick("testArgs", linkEqABC, "THE NUM"))
+                .addStick(new Stick("testArgs", eqABC, "NUM"))
+                .addStick(new Stick("testArgs", anyArg, "ANY"))
+                .addStick(new Stick("testVoid", anyArg, null));
         System.out.println(test.sayHello());
         System.out.println(test.returnInt());
-        System.out.println(test.testArgs(2));
+        System.out.println(test.testArgs(1)); // doesn't match by link or equals
+        System.out.println(test.testArgs(2<<10)); // doesn't match by link 
+        System.out.println(test.testArgs(num)); // match by link
         test.testVoid(2);
     }
 
