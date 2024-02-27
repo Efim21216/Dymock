7ec43baebc73d857b2132cf3d289e98bd5e33324
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2024-02-27 13:42:47 +0700

    Add match method and fix stab for premitive types

--- src/main/java/ru/nsu/fit/dymock/Interceptor.java
+++ src/main/java/ru/nsu/fit/dymock/Interceptor.java
@@ -1,9 +1,11 @@
 package ru.nsu.fit.dymock;
 
 import net.bytebuddy.implementation.bind.annotation.*;
+import net.bytebuddy.implementation.bytecode.constant.ClassConstant;
 
 import java.lang.reflect.Method;
 import java.util.List;
+import java.lang.reflect.Array;
 
 public class Interceptor {
     private final List<Stick> callDetails;
@@ -17,14 +19,27 @@ public class Interceptor {
                          @AllArguments Object[] arguments) {
         String methodName = invokedMethod.getName();
         System.out.println(methodName + " was involved");
-        Stick result = callDetails.stream().filter(stick -> stick.getMethodName()
-                .equals(methodName)).findAny().orElse(null);
+        Stick result = callDetails.stream()
+                                    .filter(
+                                        stick -> stick.getMethodName().equals(methodName) 
+                                        && stick.matchesLeaves(arguments))
+                                    .findAny().orElse(null);
         if (result != null) {
             return result.getResult();
         }
-        return invokedMethod.getDefaultValue();
+
+        var returnType = invokedMethod.getReturnType();
+        if(!returnType.equals(Void.TYPE)){
+            return getDefaultValue(returnType);
+        }
+
+        return null;
     }
 
+    private static <T> T getDefaultValue(Class<T> clazz) {
+        return (T) Array.get(Array.newInstance(clazz, 1), 0);
+    }
+ 
     public void addStick(Stick stick) {
         callDetails.add(stick);
     }
--- src/main/java/ru/nsu/fit/dymock/Stick.java
+++ src/main/java/ru/nsu/fit/dymock/Stick.java
@@ -3,14 +3,27 @@ package ru.nsu.fit.dymock;
 public class Stick {
     private String methodName;
 
-    private LeafMatcher[] arguments;
+    private LeafMatcher[] leaves;
 
     private final Object result;
 
     public Stick(String methodName, LeafMatcher[] arguments, Object result) {
         this.methodName = methodName;
-        this.arguments = arguments;
+        this.leaves = arguments;
         this.result = result;
+    }
+
+    public boolean matchesLeaves(Object[] arguments){
+        if(arguments.length != leaves.length){
+            return false;
+        }
+        for(int i = 0; i < leaves.length; i++){
+            if(!leaves[i].matches(arguments[i])){
+                return false;
+            }
+        }
+        
+        return true;
     }
 
     public String getMethodName() {
