7f53b4384e81cf6df397649c10288f3ea4311aff
Efim21216 <e.pashko@g.nsu.ru>
2024-03-26 14:30:22 +0700

    project demo

---
+++ src/test/java/ru/nsu/fit/demo/CallCounting.java
@@ -0,0 +1,15 @@
+package ru.nsu.fit.demo;
+
+public class CallCounting {
+    public int echoInt() {
+        return 42;
+    }
+    public int echoInt(int a) {
+        return a;
+    }
+    public int echoInt(int a, int b) {
+        return a + b;
+    }
+    public void doSomething() {
+    }
+}
